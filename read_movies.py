f11c24fe159f12c2a2e1e4f6e2b36734ad092afd
Reece Dickerson <reece.dickerson@drake.edu>
2026-03-03 22:19:06 -0600

    update read_movies.py

--- read_movies.py
+++ read_movies.py
@@ -22,10 +22,14 @@ def print_movie(movie):
     title = movie.get("Title", "Unknown Title")
     year = movie.get("Year", "Unknown Year")
     ratings = movie.get("Ratings", "No ratings")
+    director = movie.get("Director", "Unknown Director")
+    genre = movie.get("Genre", "Unknown Genre")
 
     print(f"  Title  : {title}")
     print(f"  Year   : {year}")
     print(f"  Ratings: {ratings}")
+    print(f"  Director: {director}")
+    print(f"  Genre  : {genre}")
     print()
 
 
